e3c00b11db183ee615c70b264e02800d38db6c6f
Yoelmis Enrique Perdomo Pardo <yoepardo@gmail.com>
2025-07-14 12:36:52 -0500

    create crudEvents.js
    the VISITOR role view was made

---
+++ app/controllers/crudEvents.js
@@ -0,0 +1,22 @@
+const eventsApi = "http://localhost:5000/events";
+
+export async function showVisitors() {
+    const resp = await fetch(`${eventsApi}?is_active=true`);
+    const events = await resp.json();
+    const tbody = document.querySelector("#table-events tbody");
+    tbody.innerHTML = "";
+
+    events.forEach(event => {
+        const fila = document.createElement("tr");
+        fila.innerHTML = `
+            <td>${event.name}</td>
+            <td>${event.description}</td>
+            <td>${event.capacity}</td>
+            <td>${event.date}</td>
+            <td>
+              <button onclick="recoverTask(${event.id}, this)">registrarse</button>
+            </td>
+            `;
+        tbody.appendChild(fila);
+    });
+}
